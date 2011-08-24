edaf9d7fa023a85cb5b2c2f23ddc38523b67510d
RichaBhardwaj <vinsol@Richas-MacBook-Pro.local>
2011-08-24 18:32:15 +0530

    memcache store

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,12 +17,12 @@ set :user, 'ubuntu'
 set :use_sudo, false
 set :deploy_to, "/home/ubuntu/apps/jsonObj"
 
-set :location1, "ec2-50-16-156-216.compute-1.amazonaws.com"
+set :location1, "ec2-50-19-237-144.compute-1.amazonaws.com"
 set :location2, "ec2-174-129-113-128.compute-1.amazonaws.com"
 role :web,      location1, location2
 role :app,      location1, location2
 role :db,       location1, location2, :primary => true
-role :memcache, "jsoncache.6hkbzr.0001.use1.cache.amazonaws.com"
+role :memcache, location1, location2
 
 set :rails_env, "production"
 
